d7f5753f97cc9c68d10d75e5788fa816d74ba87a
J J <klomb@tuta.io>
2020-10-26 15:45:43 -0700

    Revert "replaced SideModifier/Orientation with Direction Flags, Refactored code around those changes"
    This reverts commit 1ea043971ef27574a0160ffb8b362bcdf2aaa971.

--- SlideSharp/Container.cs
+++ SlideSharp/Container.cs
@@ -7,19 +7,79 @@ using WpfScreenHelper;
 
 namespace SlideSharp
 {
-    [Flags]
-    public enum Direction
+    public enum Orientation
     {
-        Up = 1,
-        Down = 2,
-        Left = 4,
-        Right = 8
+        Horizontal,
+        Vertical
+    }
+
+    public enum SideModifier : int
+    {
+        Positive = 1,
+        Negative = -1
     }
 
     public enum Status : int
     {
         Hiding = 1,
         Showing = -1
+    }
+
+    public class MoveIterator
+    {
+        private POINT Distance;
+        private POINT Start;
+        private POINT Step;
+
+        public MoveIterator(POINT start, POINT end, POINT maxStep)
+        {
+            Start = start;
+            Distance = start - end;
+            Step = maxStep;
+        }
+
+        public bool CanTraverse()
+        {
+            return Start + Distance != Start;
+        }
+
+        public POINT Traverse()
+        {
+            
+            var NextStep = new POINT(Math.Clamp(Distance.X, -1 * Step.X, Step.X), Math.Clamp(Distance.Y, -1 * Step.Y, Step.Y));
+            Debug.Write($"Distance Left: {Distance}, Start {Start} + ");
+            Start = Start + NextStep;
+            Distance = Distance - NextStep;
+            Debug.WriteLine($"{NextStep} = {Start}");
+            return Start;
+        }
+    }
+
+    public class Container
+    {
+        public Container(Screen screen)
+        {
+            CanBeDisposed = false;
+            Screen = screen;
+        }
+
+        public bool CanBeDisposed { get; protected set; }
+        public WindowObj ContainedWindow { get; protected set; }
+        public MoveIterator Path { get; protected set; }
+        public Screen Screen { get; }
+
+        public void UpdatePosition()
+        {
+            if (ContainedWindow.Exists() && Path.CanTraverse()) {
+                ContainedWindow.SetPosition(Path.Traverse());
+            }
+        }
+
+        public void RemoveWindow()
+        {
+            ContainedWindow = null;
+            Path = default;
+        }
     }
 
     public class CenterContainer : Container
@@ -45,106 +105,27 @@ namespace SlideSharp
         }
     }
 
-    public class Container
-    {
-        public Container(Screen screen)
-        {
-            CanBeDisposed = false;
-            Screen = screen;
-        }
-
-        public bool CanBeDisposed { get; protected set; }
-        public WindowObj ContainedWindow { get; protected set; }
-        public MoveIterator Path { get; protected set; }
-        public Screen Screen { get; }
-
-        public void RemoveWindow()
-        {
-            ContainedWindow = null;
-            Path = default;
-        }
-
-        public void UpdatePosition()
-        {
-            if (ContainedWindow.Exists() && Path?.CanTraverse() == true) {
-                ContainedWindow.SetPosition(Path.Traverse());
-            }
-        }
-    }
     public class EdgeContainer : Container
     {
-        public EdgeContainer(Direction direction, Screen screen) : base(screen)
+        public EdgeContainer(Orientation orientation, SideModifier sideModifier, Screen screen) : base(screen)
         {
-            Direction = direction;
+            Orientation = orientation;
+            SideModifier = sideModifier;
             Status = Status.Showing;
-            ScreenEdge = (Direction) switch
+            ScreenEdge = (orientation, sideModifier) switch
             {
-                Direction.Up => new RECT(Screen.WorkingArea.Left + 1, Screen.WorkingArea.Top - 1, Screen.WorkingArea.Right - 1, Screen.WorkingArea.Top + 1),
-                Direction.Down => new RECT(Screen.WorkingArea.Left + 1, Screen.WorkingArea.Bottom - 1, Screen.WorkingArea.Right - 1, Screen.WorkingArea.Bottom + 1),
-                Direction.Left => new RECT(Screen.WorkingArea.Left - 1, Screen.WorkingArea.Top + 1, Screen.WorkingArea.Left + 1, Screen.WorkingArea.Bottom -1),
-                Direction.Right => new RECT(Screen.WorkingArea.Right - 1, Screen.WorkingArea.Top + 1, Screen.WorkingArea.Right + 1, Screen.WorkingArea.Bottom - 1),
+                (Orientation.Horizontal, SideModifier.Positive) => new RECT(Screen.Bounds.Right - 1, Screen.Bounds.Top + 1, Screen.Bounds.Right + 1, Screen.Bounds.Bottom - 1),
+                (Orientation.Horizontal, SideModifier.Negative) => new RECT(Screen.Bounds.Left - 1, Screen.Bounds.Top + 1, Screen.Bounds.Left + 1, Screen.Bounds.Bottom - 1),
+                (Orientation.Vertical, SideModifier.Positive) => new RECT(Screen.Bounds.Left + 1, Screen.Bounds.Bottom - 1, Screen.Bounds.Right - 1, Screen.Bounds.Bottom + 1),
+                (Orientation.Vertical, SideModifier.Negative) => new RECT(Screen.Bounds.Left + 1, Screen.Bounds.Top - 1, Screen.Bounds.Right - 1, Screen.Bounds.Top + 1),
                 _ => throw new InvalidOperationException(),
             };
         }
 
-        public Direction Direction { get; private set; }
-        public RECT ScreenEdge { get; private set; }
+        public Orientation Orientation { get; }
+        public SideModifier SideModifier { get; }
         public Status Status { get; private set; }
-        public static List<EdgeContainer> GetAllValidInstances()
-        {
-            List<EdgeContainer> NewList = new List<EdgeContainer>();
-            Screen.AllScreens.ToList().ForEach((S) => {
-                foreach (Direction d in Enum.GetValues(typeof(Direction))) {
-                    EdgeContainer edge = GetValidInstance(d, S);
-                    if (edge != null) {
-                        NewList.Add(edge);
-                    }
-                }
-            });
-            return NewList;
-        }
-
-        public static EdgeContainer GetValidInstance(Direction direction, Screen screen)
-        {
-            var NewInstance = new EdgeContainer(direction, screen);
-            return Screen.AllScreens.ToList().Exists((S) => {
-                if (S == NewInstance.Screen) {
-                    return false;
-                } else {
-                    return S.Bounds.Contains(NewInstance.ScreenEdge.ToWindowsRect());
-                }
-            }) ? null : NewInstance;
-        }
-
-        public bool Intersect(POINT start, POINT end)
-        {
-            POINT VecFromStartEnd = start - end;
-            Direction vectorDirection = new Direction();
-            if (VecFromStartEnd.X != 0) {
-                vectorDirection |= (VecFromStartEnd.X > 0 ? Direction.Left : Direction.Right);
-            }
-            if (VecFromStartEnd.Y != 0) {
-                vectorDirection |= (VecFromStartEnd.Y > 0 ? Direction.Up : Direction.Down);
-            }
-            if ((vectorDirection & Direction) != Direction) {
-                return false;
-            }
-
-            var DifOfScreenToStart = ScreenEdge.Center - start;
-            var divs = (Direction) switch
-            {
-                Direction.Left => (double)DifOfScreenToStart.X / (double)VecFromStartEnd.X,
-                Direction.Up => (double)DifOfScreenToStart.Y / (double)VecFromStartEnd.Y,
-                Direction.Right => (double)DifOfScreenToStart.X / (double)VecFromStartEnd.X,
-                Direction.Down => (double)DifOfScreenToStart.Y / (double)VecFromStartEnd.Y,
-                _ => throw new InvalidOperationException(),
-            };
-
-            var PointAtEnd = new POINT(divs * VecFromStartEnd.X, divs * VecFromStartEnd.Y);
-            var EndPoint = start + PointAtEnd;
-            Debug.WriteLine($"{ScreenEdge.Contains(EndPoint)}, @{EndPoint} Wants:{ScreenEdge}");
-            return ScreenEdge.Contains(EndPoint);
-        }
+        public RECT ScreenEdge { get; private set; }
 
         public void SetNewWindow(IntPtr windowHandle)
         {
@@ -174,25 +155,60 @@ namespace SlideSharp
             POINT maxStep = new POINT(30, 30); //temporary until i get configs
             int offSet = Status == Status.Showing ? 0 : 30;
             POINT NextPoint = new POINT(
-                ScreenEdge.Center.X - ContainedWindow.WindowArea.Width / 2,
-                ScreenEdge.Center.Y - ContainedWindow.WindowArea.Height / 2);
+                ScreenEdge.Center.X - (ContainedWindow.WindowArea.Width / 2),
+                ScreenEdge.Center.Y - (ContainedWindow.WindowArea.Height / 2));
 
-            switch (Direction) {
-                case Direction.Up:
-                    break;
-
-                case Direction.Down:
-                    break;
-
-                case Direction.Left:
-                    break;
-
-                case Direction.Right:
-                    break;
+            if (Orientation == Orientation.Horizontal) {
+                NextPoint.X += ((int)Status * (int)SideModifier * (ContainedWindow.WindowArea.Width / 2)) + ((int)SideModifier * offSet);
+            } else if(Orientation == Orientation.Vertical) {
+                NextPoint.Y += ((int)Status * (int)SideModifier * (ContainedWindow.WindowArea.Height / 2)) + ((int)SideModifier * offSet);
             }
-
-            Debug.WriteLine($"{ContainedWindow.WindowArea.ToPoint()} {NextPoint} {ScreenEdge.Center}");
+            Debug.WriteLine($"{NextPoint} {ScreenEdge.Center}");
             Path = new MoveIterator(ContainedWindow.WindowArea.ToPoint(), NextPoint, maxStep);
+        }
+
+        public bool Intersect(POINT start, POINT end)
+        {
+            var DifOfScreenToStart = ScreenEdge.Center - start;
+            var VecFromStartEnd = start - end;
+            var divs = (Orientation) switch
+            {
+                Orientation.Horizontal => (double)DifOfScreenToStart.X / (double)VecFromStartEnd.X,
+                Orientation.Vertical => (double)DifOfScreenToStart.Y / (double)VecFromStartEnd.Y,
+                _ => throw new InvalidOperationException(),
+            };
+            var PointAtEnd = new POINT(divs * VecFromStartEnd.X, divs * VecFromStartEnd.Y);
+            var EndPoint = start + PointAtEnd;
+
+            return ScreenEdge.Contains(EndPoint);
+        }
+
+        public static EdgeContainer GetValidInstance(Orientation orientation, SideModifier sideModifier, Screen screen)
+        {
+            var NewInstance = new EdgeContainer(orientation, sideModifier, screen);
+            return Screen.AllScreens.ToList().Exists((S) => {
+                if (S == NewInstance.Screen) {
+                    return false;
+                } else {
+                    return S.Bounds.Contains(NewInstance.ScreenEdge.ToWindowsRect());
+                }
+            }) ? null : NewInstance;
+        }
+
+        public static List<EdgeContainer> GetAllValidInstances()
+        {
+            List<EdgeContainer> NewList = new List<EdgeContainer>();
+            Screen.AllScreens.ToList().ForEach((S) => {
+                foreach(Orientation o in Enum.GetValues(typeof(Orientation))) {
+                    foreach(SideModifier sm in Enum.GetValues(typeof(SideModifier))) {
+                        EdgeContainer edge = GetValidInstance(o, sm, S);
+                        if (edge != null) {
+                            NewList.Add(edge);
+                        }
+                    }
+                }
+            });
+            return NewList;
         }
     }
 }
