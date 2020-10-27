28ec4e43fb6c796c7be64f92f2bdca31595e34d4
J J <klomb@tuta.io>
2020-10-27 15:00:48 -0700

    Moved Point/Rect to own file

---
+++ Win32Api/Point.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Win32Api
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public POINT(double x, double y)
+        {
+            X = (int)x;
+            Y = (int)y;
+        }
+
+        public static POINT operator +(POINT p1, POINT p2)
+        {
+            return new POINT(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static POINT operator -(POINT p1, POINT p2)
+        {
+            return new POINT(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static bool operator ==(POINT p1, POINT p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public static POINT operator /(POINT p1, int divisor)
+        {
+            return new POINT(p1.X / divisor, p1.Y / divisor);
+        }
+
+        public static bool operator !=(POINT p1, POINT p2)
+        {
+            return !p1.Equals(p2);
+        }
+
+        public bool Equals(POINT p)
+        {
+            return p.X == X && p.Y == Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X + (19 * Y);
+        }
+
+        public override string ToString()
+        {
+            return $"*{X}:{Y}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is POINT pOINT) {
+                return Equals(pOINT);
+            } else {
+                return false;
+            }
+        }
+    }
+}
---
+++ Win32Api/Rect.cs
@@ -0,0 +1,80 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Win32Api
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left, Top, Right, Bottom;
+        public int Width => Right - Left;
+        public int Height => Bottom - Top;
+        public POINT Center => new POINT(Width / 2, Height / 2);
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            Left = left;
+            Top = top;
+            Right = right;
+            Bottom = bottom;
+        }
+
+        public RECT(double left, double top, double right, double bottom)
+        {
+            (Left, Top, Right, Bottom) = ((int)left, (int)top, (int)right, (int)bottom);
+        }
+
+        public static RECT FromRectangle(System.Drawing.Rectangle R)
+        {
+            return new RECT(R.Left, R.Top, R.Right, R.Bottom);
+        }
+
+        public POINT ToPoint()
+        {
+            return new POINT(Left, Top);
+        }
+
+        public bool Contains(POINT pt)
+        {
+            return Left <= pt.X && Right >= pt.X && Top <= pt.Y && Bottom >= pt.Y;
+        }
+
+        public static bool operator ==(RECT r1, RECT r2)
+        {
+            return r1.Equals(r2);
+        }
+
+        public static bool operator !=(RECT r1, RECT r2)
+        {
+            return !r1.Equals(r2);
+        }
+
+        public bool Equals(RECT r)
+        {
+            return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+        }
+
+        public override int GetHashCode()
+        {
+            return Left + (15 * Top) + (17 * Right) + (21 * Bottom);
+        }
+
+        public override string ToString()
+        {
+            return $"[*{Left}:{Top}, *{Right}:{Bottom}]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is RECT rECT) {
+                return Equals(rECT);
+            } else {
+                return false;
+            }
+        }
+
+        public System.Windows.Rect ToWindowsRect()
+        {
+            return new Rect(Left, Top, Width, Height);
+        }
+    }
+}
