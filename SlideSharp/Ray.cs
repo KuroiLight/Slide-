7ad998d18e64bd55e5e33e529d12b1ceab25dfea
J J <klomb@tuta.io>
2020-11-01 19:22:17 -0800

    Added custom Ray/Vector structs

---
+++ SlideSharp/Ray.cs
@@ -0,0 +1,32 @@
+﻿using Win32Api;
+
+namespace SlideSharp
+{
+    public struct Ray
+    {
+        public POINT Position { get; }
+        public Vector Direction { get; }
+
+        public Ray(POINT start, POINT end)
+        {
+            Position = start;
+            Direction = new Vector(start - end);
+        }
+
+        public Ray(POINT position, Vector direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+
+        public POINT EndPoint()
+        {
+            return new POINT(Position.X + Direction.X, Position.Y + Direction.Y);
+        }
+
+        public Ray Scale(double factor)
+        {
+            return new Ray(Position, Direction.Multiply(factor));
+        }
+    }
+}
---
+++ SlideSharp/Vector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Win32Api;
+
+namespace SlideSharp
+{
+    public struct Vector
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Vector(POINT pt)
+        {
+            X = pt.X;
+            Y = pt.Y;
+        }
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector((double x, double y) tuple)
+        {
+            (X, Y) = tuple;
+        }
+
+        public Vector(Vector v)
+        {
+            X = v.X;
+            Y = v.Y;
+        }
+
+        public Vector(POINT p1, POINT p2)
+        {
+            POINT diff = p2 - p1;
+            X = diff.X;
+            Y = diff.Y;
+        }
+
+        public Vector Clamp(int absoluteMinMax)
+        {
+            int absoluteMinMin = absoluteMinMax * -1;
+            return new Vector(this)
+            {
+                X = Math.Clamp(X, absoluteMinMin, absoluteMinMax),
+                Y = Math.Clamp(Y, absoluteMinMin, absoluteMinMax)
+            };
+        }
+
+        public Vector Multiply(double factor)
+        {
+            return new Vector(X * factor, Y * factor);
+        }
+
+        public double Length()
+        {
+            return Math.Abs(Math.Sqrt((X * X) + (Y * Y)));
+        }
+
+        public POINT ToPoint()
+        {
+            return new POINT(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return $"/{X}:{Y}/";
+        }
+    }
+}
