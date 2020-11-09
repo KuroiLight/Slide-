f8f531384a70360f17c1e5f6974fee1cdac9ad43
J J <klomb@tuta.io>
2020-11-08 19:45:15 -0800

    Moved MouseHook to its own class; Simplified UpdateStates further

--- SlideSharp/Coordinator.cs
+++ SlideSharp/Coordinator.cs
@@ -12,102 +12,84 @@ namespace SlideSharp
     public class Coordinator
     {
         private readonly DispatcherTimer Dispatcher = new DispatcherTimer();
-        private readonly IntPtr HookHandle;
         private readonly object MouseDataLock = new object();
-        private readonly HookProc MouseHookProcHandle;
-
         private Ray? _ray;
-
-        private POINT? MStart;
-        private readonly List<SlidingWindow> Windows;
-
-        public Coordinator()
-        {
-            Windows = new List<SlidingWindow>(Screen.AllScreens.Count() * 5);
-            Dispatcher.Tick += UpdateStates;
-            Dispatcher.Interval = new TimeSpan(0, 0, 0, 0, MainWindow.config.Update_Interval);
-            Dispatcher.Start();
-            MouseHookProcHandle = MouseHookProc;
-            HookHandle = SetWindowsHookEx(MouseHookProcHandle);
-        }
-
         private Ray? Ray
         {
             get
             {
-                lock (MouseDataLock)
-                {
+                lock (MouseDataLock) {
                     return _ray;
                 }
             }
             set
             {
-                lock (MouseDataLock)
-                {
+                lock (MouseDataLock) {
                     _ray = value;
                 }
             }
         }
 
+        private readonly MouseHook _mouseHook;
+
+        private POINT MStart;
+        private readonly List<SlidingWindow> Windows;
+
+        public Coordinator()
+        {
+            Windows = new List<SlidingWindow>(Screen.AllScreens.Count() * 5);
+
+            Dispatcher.Tick += UpdateStates;
+            Dispatcher.Interval = new TimeSpan(0, 0, 0, 0, Configuration.SettingsInstance.Update_Interval);
+            Dispatcher.Start();
+
+            _mouseHook = new MouseHook((int code, WM_MOUSE wParam, MSLLHOOKSTRUCT lParam) => {
+                if (wParam == WM_MOUSE.WM_MBUTTONDOWN) {
+                    MStart = lParam.pt;
+                } else if (wParam == WM_MOUSE.WM_MBUTTONUP) {
+                    Ray = new Ray((POINT)MStart, MStart - lParam.pt);
+                }
+
+
+                return CallNextHookEx(IntPtr.Zero, code, wParam, lParam);
+            });
+        }
+
         ~Coordinator()
         {
             Dispatcher.Stop();
-            UnhookWindowsHookEx(HookHandle);
-        }
-
-        private IntPtr MouseHookProc(int code, WM_MOUSE wParam, MSLLHOOKSTRUCT lParam)
-        {
-            if (wParam == WM_MOUSE.WM_MBUTTONDOWN)
-                MStart = lParam.pt;
-            else if (wParam == WM_MOUSE.WM_MBUTTONUP)
-                if (MStart != null)
-                {
-                    Ray = new Ray((POINT) MStart, lParam.pt);
-                    MStart = null;
-                }
-
-            return CallNextHookEx(HookHandle, code, wParam, lParam);
         }
 
         private void UpdateStates(object sender, EventArgs e)
         {
             Dispatcher.Stop();
 
-            var WindowUnderlMEnd = IntPtr.Zero;
-            Ray localRay = default;
-            if (Ray.HasValue)
-            {
-                localRay = Ray.Value;
-                WindowUnderlMEnd = GetRootWindowIf(localRay.Position,
-                    hwnd => GetTitleBarInfo(hwnd).rcTitleBar.Contains(localRay.Position));
-                Ray = null;
+            Ray localRay = Ray;
+            Ray = null;
+
+            if (localRay != null) {
+                IntPtr capturedWindow;
+
+                if ((capturedWindow = GetRootWindowFromTitlebar(localRay.Position)) != IntPtr.Zero) {
+                    Windows.Find(window => window.HasWindow(capturedWindow)).MarkForDeletion();
+                    var toSlider = SlidingWindow.CreateFromRay(localRay);
+                    toSlider.ManageWindow(capturedWindow);
+                    Windows.Add(toSlider);
+                }
             }
 
+            Windows.RemoveAll(window => window.MarkedForDeletion);
+
             var WindowUnderCursor = GetRootWindow(GetCursorPos());
-            var MousePos = GetCursorPos();
 
-
-            Windows.AsParallel().ForAll(Window =>
-            {
+            Windows.ForEach(Window => {
                 if (Window.HasWindow(WindowUnderCursor))
                     Window.SetWindowState(Status.Showing);
                 else
                     Window.SetWindowState(Status.Hiding);
 
-                if (Window.HasWindow(WindowUnderlMEnd))
-                    Window.MarkForDeletion();
-                else
-                    Window.MoveNextStep();
+                Window.MoveNextStep();
             });
-
-            if (WindowUnderlMEnd != IntPtr.Zero)
-            {
-                var toSlider = SlidingWindow.CreateFromRay(localRay);
-                toSlider.ManageWindow(WindowUnderlMEnd);
-                Windows.Add(toSlider);
-            }
-
-            Windows.RemoveAll(window => window.MarkedForDeletion);
 
             Dispatcher.Start();
         }
