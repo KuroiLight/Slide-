97374fdb51b96e89ca4b346558826a4bd039350d
J J <klomb@tuta.io>
2020-11-08 19:44:20 -0800

    Moved MouseHook to its own class

---
+++ SlideSharp/MouseHook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Win32Api;
+using static Win32Api.Imports;
+
+namespace SlideSharp
+{
+    public class MouseHook
+    {
+        private readonly HookProc _mouseHookProcHandle;
+        private readonly IntPtr _hookHandle;
+
+        public MouseHook(HookProc hookProc)
+        {
+            _mouseHookProcHandle = hookProc;
+            _hookHandle = User32.SetWindowsHookEx(_mouseHookProcHandle);
+            if (_hookHandle == IntPtr.Zero) {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        ~MouseHook()
+        {
+            User32.UnhookWindowsHookEx(_hookHandle);
+        }
+    }
+}
