5b583bf0c6ba25fe75dcd6e08ea40ad0d577beed
J J <klomb@tuta.io>
2020-11-13 09:36:57 -0800

    Fixed bad Position calculations; Removed Direction field

--- SlideSharp/Slide.cs
+++ SlideSharp/Slide.cs
@@ -6,7 +6,6 @@ namespace SlideSharp
 {
     public abstract class Slide
     {
-        public Direction Direction;
         public Screen Screen;
         protected RECT _screen;
 
@@ -24,7 +23,6 @@ namespace SlideSharp
 
     public class CenterSlide : Slide
     {
-        public new Direction Direction = Direction.Center;
         public CenterSlide(Screen screen) : base(screen) { }
 
         public override POINT ShownPosition(RECT WindowRect)
@@ -40,61 +38,57 @@ namespace SlideSharp
 
     public class LeftSlide : Slide
     {
-        public new Direction Direction = Direction.Left;
         public LeftSlide(Screen screen) : base(screen) { }
         public override POINT ShownPosition(RECT WindowRect)
         {
-            return new POINT(_screen.Left, _screen.Center.Y - WindowRect.Center.Y);
+            return new POINT(_screen.Left, _screen.Center.Y - WindowRect.Height / 2);
         }
 
         public override POINT HiddenPosition(RECT WindowRect)
         {
-            return new POINT((_screen.Left - WindowRect.Width) + 30, _screen.Center.Y - WindowRect.Center.Y);
+            return new POINT((_screen.Left - WindowRect.Width) + Configuration.SettingsInstance.Window_Offscreen_Offset, _screen.Center.Y - WindowRect.Height / 2);
         }
     }
 
     public class RightSlide : Slide
     {
-        public new Direction Direction = Direction.Right;
         public RightSlide(Screen screen) : base(screen) { }
         public override POINT ShownPosition(RECT WindowRect)
         {
-            return new POINT(_screen.Right - WindowRect.Width, _screen.Center.Y - WindowRect.Center.Y);
+            return new POINT(_screen.Right - WindowRect.Width, _screen.Center.Y - WindowRect.Height / 2);
         }
 
         public override POINT HiddenPosition(RECT WindowRect)
         {
-            return new POINT(_screen.Right - Configuration.SettingsInstance.Window_Offscreen_Offset, _screen.Center.Y - WindowRect.Center.Y);
+            return new POINT(_screen.Right - Configuration.SettingsInstance.Window_Offscreen_Offset, _screen.Center.Y - WindowRect.Height / 2);
         }
     }
 
     public class TopSlide : Slide
     {
-        public new Direction Direction = Direction.Up;
         public TopSlide(Screen screen) : base(screen) { }
         public override POINT ShownPosition(RECT WindowRect)
         {
-            return new POINT(_screen.Center.X - WindowRect.Center.X, _screen.Top);
+            return new POINT(_screen.Center.X - WindowRect.Width / 2, _screen.Top);
         }
 
         public override POINT HiddenPosition(RECT WindowRect)
         {
-            return new POINT(_screen.Center.X - WindowRect.Center.X, (_screen.Top - WindowRect.Height) + Configuration.SettingsInstance.Window_Offscreen_Offset);
+            return new POINT(_screen.Center.X - WindowRect.Width / 2, (_screen.Top - WindowRect.Height) + Configuration.SettingsInstance.Window_Offscreen_Offset);
         }
     }
 
     public class BottomSlide : Slide
     {
-        public new Direction Direction = Direction.Down;
         public BottomSlide(Screen screen) : base(screen) { }
         public override POINT ShownPosition(RECT WindowRect)
         {
-            return new POINT(_screen.Center.X - WindowRect.Center.X, _screen.Bottom - WindowRect.Height);
+            return new POINT(_screen.Center.X - WindowRect.Width / 2, _screen.Bottom - WindowRect.Height);
         }
 
         public override POINT HiddenPosition(RECT WindowRect)
         {
-            return new POINT(_screen.Center.X - WindowRect.Center.X, _screen.Bottom - Configuration.SettingsInstance.Window_Offscreen_Offset);
+            return new POINT(_screen.Center.X - WindowRect.Width / 2, _screen.Bottom - Configuration.SettingsInstance.Window_Offscreen_Offset);
         }
     }
 }
