68d472bce5880e10d429bae505ec8ae718a1c114
J J <klomb@tuta.io>
2020-11-13 17:46:59 -0800

    Added QuarticEase to Easer

--- SlideSharp/Easer.cs
+++ SlideSharp/Easer.cs
@@ -1,31 +1,39 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Windows.Media.Animation;
 using Win32Api;
 
 namespace SlideSharp
 {
     public struct Easer
     {
+        private const double MaxPercent = 100.0;
+        private double Percent;
+        private readonly QuarticEase QuarticEase;
         private POINT StartingPoint;
         private readonly double StepSizeX, StepSizeY;
-        public double Percent;
 
 
         public Easer(POINT start, POINT end)
         {
             StartingPoint = start;
             var dist = end - start;
-            StepSizeX = dist.X / 100.0;
-            StepSizeY = dist.Y / 100.0;
+            StepSizeX = dist.X / MaxPercent;
+            StepSizeY = dist.Y / MaxPercent;
             Percent = 0;
+            QuarticEase = new QuarticEase();
+            QuarticEase.EasingMode = EasingMode.EaseOut;
         }
 
         public POINT TakeStep()
         {
-            Percent++;
-            double X, Y;
-            X = StepSizeX * Percent;
-            Y = StepSizeY * Percent;
-            return StartingPoint + new POINT(X, Y);
+            Percent += 0.025;
+            var easedValue = QuarticEase.Ease(Percent) * MaxPercent;
+            return StartingPoint + new POINT(StepSizeX * easedValue, StepSizeY * easedValue);
+        }
+
+        public bool CanMove()
+        {
+            return Math.Round(Percent * MaxPercent) < MaxPercent;
         }
     }
 }
