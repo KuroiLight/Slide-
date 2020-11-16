4398d31296d9806af283879750355711cb2451ff
J J <klomb@tuta.io>
2020-11-15 17:40:15 -0800

    using ExpoOut ease instead of built-in quartic

--- SlideSharp/Easer.cs
+++ SlideSharp/Easer.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Windows.Media.Animation;
 using Win32Api;
 
 namespace SlideSharp
 {
     public struct Easer
     {
-        private const double MaxPercent = 100.0;
+        private const double MaxPercent = 100.0f;
         private double Percent;
-        private readonly QuarticEase QuarticEase;
         private POINT StartingPoint;
         private readonly double StepSizeX, StepSizeY;
 
@@ -20,20 +18,22 @@ namespace SlideSharp
             StepSizeX = dist.X / MaxPercent;
             StepSizeY = dist.Y / MaxPercent;
             Percent = 0;
-            QuarticEase = new QuarticEase();
-            QuarticEase.EasingMode = EasingMode.EaseOut;
         }
 
         public POINT TakeStep()
         {
             Percent += Configuration.Config.WINDOW_ANIM_SPEED;
-            var easedValue = QuarticEase.Ease(Percent) * MaxPercent;
+            var easedValue = Out(Percent) * MaxPercent;
             return StartingPoint + new POINT(StepSizeX * easedValue, StepSizeY * easedValue);
         }
 
         public bool CanMove()
         {
             return Math.Round(Percent * MaxPercent) < MaxPercent;
+        }
+        public static double Out(double k)
+        {
+            return k == 1.0 ? 1.0 : 1.0 - Math.Pow(2.0, -10.0 * k);
         }
     }
 }
