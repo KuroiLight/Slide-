566b5e3de04afcb9fef6a3d00a25ab4290536889
J J <klomb@tuta.io>
2020-11-21 07:57:15 -0800

    Added Drawing.Rectangle Extensions

---
+++ SlideSharp/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace ExtensionMethods
+{
+    public static class RectangleMethods
+    {
+        public static Point TopLeft(this Rectangle rc)
+        {
+            return new Point(rc.X, rc.Y);
+        }
+        public static Point TopRight(this Rectangle rc)
+        {
+            return new Point(rc.X + rc.Width, rc.Y);
+        }
+        public static Point BottomLeft(this Rectangle rc)
+        {
+            return new Point(rc.X, rc.Y + rc.Height);
+        }
+        public static Point BottomRight(this Rectangle rc)
+        {
+            return new Point(rc.X + rc.Width, rc.Y + rc.Height);
+        }
+    }
+}
