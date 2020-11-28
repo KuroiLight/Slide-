f0f61f6e19bfa30f9de29b12a33dde69dfcf789f
J J <klomb@tuta.io>
2020-11-28 11:00:21 -0800

    using cleanup

--- SlideSharp/MouseHook.cs
+++ SlideSharp/MouseHook.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows;
 using Win32Api;
@@ -17,7 +16,7 @@ namespace SlideSharp
         {
             _mouseHookProcHandle = hookProc;
             _hookHandle = User32.SetWindowsHookEx(_mouseHookProcHandle);
-            if(_hookHandle == IntPtr.Zero) {
+            if (_hookHandle == IntPtr.Zero) {
                 MessageBox.Show($"{new Win32Exception(Marshal.GetLastWin32Error()).Message}\nwith no MouseHook application must now close.");
                 Application.Current.Shutdown();
             }
