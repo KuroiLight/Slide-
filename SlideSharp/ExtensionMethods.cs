c51345bdd247b0b1a26d1b8627b6d5bcf5f5e41d
J J <klomb@tuta.io>
2020-12-04 10:46:48 -0800

    Removed need for Ray class; Reduced switches and overwall code in SlideFactory; Added HasPoint to Rectangle methods

--- SlideSharp/ExtensionMethods.cs
+++ SlideSharp/ExtensionMethods.cs
@@ -23,5 +23,16 @@ namespace ExtensionMethods
         {
             return new Point(rc.X + rc.Width, rc.Y + rc.Height);
         }
+
+        /// <summary>
+        /// Similar to Contains, but in addition will return true if point is on rectangles edge
+        /// </summary>
+        /// <param name="rc">rectangle being tested</param>
+        /// <param name="pt">point to look for</param>
+        /// <returns>true if point is on or inside of the rectangle, otherwise false</returns>
+        public static bool HasPoint(this Rectangle rc, Point pt)
+        {
+            return rc.X <= pt.X && rc.Right >= pt.X && rc.Y <= pt.Y && rc.Bottom >= pt.Y;
+        }
     }
 }
--- SlideSharp/SlideFactory.cs
+++ SlideSharp/SlideFactory.cs
@@ -2,81 +2,63 @@
 using Screen_Drop_In;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using Win32Api;
 
 namespace SlideSharp
 {
     public static class SlideFactory
     {
-        private static Direction[] GetDirections(Direction flag)
+        public static Slide? SlideFromMouseMovement(POINT start, POINT end)
         {
-            var AllValues = Enum.GetNames(typeof(Direction));
-            List<Direction> ds = new();
+            var screen = Screen.FromPoint(end.ToDrawingPoint());
+            if (screen is null) return null;
 
-            foreach (var item in AllValues)
+            var diff = end - start;
+            if (diff.LengthAsVector() < Config.GetInstance.MouseDragDeadzone) return new CenterSlide(screen);
+
+            static System.Drawing.Point scaleVector(POINT _start, POINT _vector, double _factor) 
             {
-                var parsed = Enum.Parse<Direction>(item);
-                if ((flag & parsed) != 0) ds.Add(parsed);
+                var temp = (_vector * _factor) + _start;
+                return new Point(temp.X, temp.Y);
             }
 
-            return ds.ToArray();
-        }
-
-        public static Slide SlideFromRay(Ray ray)
-        {
-            Screen? screen = Screen.FromPoint(ray.EndPoint().ToDrawingPoint());
-            if (screen is null) return new CenterSlide(Screen.PrimaryScreen);
-            if (ray.Movement.LengthAsVector() < Config.GetInstance.MouseDragDeadzone) return new CenterSlide(screen);
-
-            return (GetActualDirection(ray, screen)) switch
+            if (diff.X > 0 && IsValidSlideDirection(Direction.Left, screen) && screen.WorkingArea.HasPoint(scaleVector(start, diff, (double)(screen.Bounds.Right - start.X) / diff.X))) // right
             {
-                Direction.Up when IsValidSlideDirection(Direction.Up, screen) => new TopSlide(screen),
-                Direction.Down when IsValidSlideDirection(Direction.Down, screen) => new BottomSlide(screen),
-                Direction.Left when IsValidSlideDirection(Direction.Left, screen) => new LeftSlide(screen),
-                Direction.Right when IsValidSlideDirection(Direction.Right, screen) => new RightSlide(screen),
-                _ => new CenterSlide(screen)
-            };
+                return new RightSlide(screen);
+            }
+            else if (diff.X < 0 && IsValidSlideDirection(Direction.Right, screen) && screen.WorkingArea.HasPoint(scaleVector(start, diff, (double)(screen.Bounds.Left - start.X) / diff.X))) // left
+            {
+                return new LeftSlide(screen);
+            }
+
+            if (diff.Y < 0 && IsValidSlideDirection(Direction.Up, screen) && screen.WorkingArea.HasPoint(scaleVector(start, diff, (double)(screen.Bounds.Top - start.Y) / diff.Y))) // top
+            {
+                return new TopSlide(screen);
+            }
+            else if (diff.Y > 0 && IsValidSlideDirection(Direction.Down, screen) && screen.WorkingArea.HasPoint(scaleVector(start, diff, (double)(screen.Bounds.Bottom - start.Y) / diff.Y))) // bottom
+            {
+                return new BottomSlide(screen);
+            }
+
+            return new CenterSlide(screen);
         }
 
         private static bool IsValidSlideDirection(Direction dir, Screen screen)
         {
             static System.Drawing.Point PointOffset(System.Drawing.Point pt, int x, int y) => new(pt.X + x, pt.Y + y);
 
-            System.Drawing.Point outsidePt = (dir) switch
+            System.Drawing.Point? outsidePt = (dir) switch
             {
                 Direction.Up => PointOffset(screen.WorkingArea.TopLeft(), screen.Bounds.Width / 2, -100),
                 Direction.Down => PointOffset(screen.WorkingArea.BottomLeft(), screen.Bounds.Width / 2, 100),
                 Direction.Left => PointOffset(screen.WorkingArea.TopLeft(), -100, screen.Bounds.Height / 2),
                 Direction.Right => PointOffset(screen.WorkingArea.TopRight(), 100, screen.Bounds.Height / 2),
-                _ => default,
+                _ => null,
             };
 
-            foreach (var scr in Screen.AllScreens)
-            {
-                if (scr.WorkingArea.Contains(outsidePt)) return false;
-            }
-            return true;
-        }
-
-        private static Direction GetActualDirection(Ray ray, Screen screen)
-        {
-            foreach (Direction flag in GetDirections(ray.Direction))
-            {
-                POINT? endPoint = flag switch
-                {
-                    Direction.Up => ray.ScaledEndPoint((screen.Bounds.Top - ray.Position.Y) / ray.Movement.Y),
-                    Direction.Down => ray.ScaledEndPoint((screen.Bounds.Bottom - ray.Position.Y) / ray.Movement.Y),
-                    Direction.Left => ray.ScaledEndPoint((screen.Bounds.Left - ray.Position.X) / ray.Movement.X),
-                    Direction.Right => ray.ScaledEndPoint((screen.Bounds.Right - ray.Position.X) / ray.Movement.X),
-                    _ => null
-                };
-
-                if (endPoint.HasValue && screen.Bounds.Contains(endPoint.Value.ToDrawingPoint()))
-                {
-                    return flag;
-                }
-            }
-            return Direction.Center;
+            if (outsidePt is null) return false;
+            return Screen.FromPoint(outsidePt.Value) == null;
         }
     }
 }
